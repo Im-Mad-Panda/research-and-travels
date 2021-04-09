0dceed122bf38291a0ea6ca9fe466deffe4f0144
Daniil <79607193+Im-Mad-Panda@users.noreply.github.com>
2021-04-10 01:25:45 +0300

    slide-experiment

--- js/app.js
+++ js/app.js
@@ -1,16 +1,10 @@
-const testimonialsSlider = new Swiper('.testimonials-slider', {
-
-    breakpoints: {
-          slidesPerView: 'auto',
-          centeredSlides: true,
-        },
-
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'progressbar',
-    },
-    navigation: {
-    nextEl: '.arrow-left',
-    prevEl: '.arrow-right',
+const swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
   },
-  });
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
